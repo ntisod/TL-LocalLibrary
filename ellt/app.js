80484e654c5a460014f31d3a12824635b40d897c
Tobias Lindberg (Dell) <tobias.lindberg@sodertalje.ntig.se>
2018-03-05 14:33:41 +0100

    Del 4

--- ellt/app.js
+++ ellt/app.js
@@ -7,12 +7,15 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://user1:user1234@ds012198.mlab.com:12198/t-local-library';
-mongoose.connect(mongoDB);
+var mongoDB = 'mongodb://nodeuser:Mpfr28@ds012198.mlab.com:12198/t-local-library';
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -31,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);  // Add catalog routes to middleware chain.
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
